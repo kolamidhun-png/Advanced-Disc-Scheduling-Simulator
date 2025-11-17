15cb114f2193cdfcc760c75c50816491db4f8c51
Kola Midhun Kumar <kolamidhun@gmail.com>
2025-11-17 18:52:21 +0530

    Refactor disk scheduling algorithms for clarity

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,8 @@
-"""Core disk scheduling implementations.
+import math
+return self._metrics(order)
+
+
+def cscan(self, direction: str = "up"):
 if not self.requests:
 return self._metrics([])
 size = self.disk_size
@@ -6,18 +10,17 @@ left = sorted([r for r in self.requests if r < self.head])
 right = sorted([r for r in self.requests if r >= self.head])
 order = []
 if direction == "up":
-order.extend(right)
+order += right
 if size is not None:
-# sweep to end, then jump to start (we include end and start as traversal points)
 order.append(size - 1)
 order.append(0)
-order.extend(left)
+order += left
 else:
-order.extend(reversed(left))
+order += list(reversed(left))
 if size is not None:
 order.append(0)
 order.append(size - 1)
-order.extend(right)
+order += right
 return self._metrics(order)
 
 
@@ -28,11 +31,11 @@ left = sorted([r for r in self.requests if r < self.head])
 right = sorted([r for r in self.requests if r >= self.head])
 order = []
 if direction == "up":
-order.extend(right)
-order.extend(reversed(left))
+order += right
+order += list(reversed(left))
 else:
-order.extend(reversed(left))
-order.extend(right)
+order += list(reversed(left))
+order += right
 return self._metrics(order)
 
 
@@ -43,12 +46,22 @@ left = sorted([r for r in self.requests if r < self.head])
 right = sorted([r for r in self.requests if r >= self.head])
 order = []
 if direction == "up":
-order.extend(right)
+order += right
 if left:
-# jump directly to the smallest left request
-order.extend(left)
+order += left
 else:
-order.extend(reversed(left))
+order += list(reversed(left))
 if right:
-order.extend(reversed(right))
+order += list(reversed(right))
 return self._metrics(order)
+
+
+def simulate_all(self, visualize: bool = True, direction: str = "up"):
+results = {}
+results["FCFS"] = self.fcfs()
+results["SSTF"] = self.sstf()
+results["SCAN"] = self.scan(direction=direction)
+results["C-SCAN"] = self.cscan(direction=direction)
+results["LOOK"] = self.look(direction=direction)
+results["C-LOOK"] = self.clook(direction=direction)
+return results
